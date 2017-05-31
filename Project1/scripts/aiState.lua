8e12039843c0655a30886f43ea606cae81af4df6
Jonathan Sands <cosmictails@gmail.com>
2017-05-31 13:22:57 +0800

    AI State Lua Script Added
    States currently do nothing. Implement them under their respective
    states.

---
+++ Project1/scripts/aiState.lua
@@ -0,0 +1,15 @@
+--aiState.lua
+state = "begin"
+function (state)
+	if state == "begin" then
+		
+	elseif state == "attack" then
+		
+	elseif state == "search" then
+		
+	elseif state == "dead" then
+		
+	else
+		print "Invalid state!"	
+	end
+end
